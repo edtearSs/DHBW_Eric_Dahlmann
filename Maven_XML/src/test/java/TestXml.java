50a55e1bccac3a83ad3914f30e46b39f33cdf157
edtearSs <eric.dahlmann@yahoo.de>
2024-11-09 22:55:11 +0100

    Added comments about the different functions in the project

--- Maven_XML/src/test/java/TestXml.java
+++ Maven_XML/src/test/java/TestXml.java
@@ -51,11 +51,14 @@ public class TestXml {
     @Test
     public void testXmlToPerson() throws JAXBException{
 
+        //Path für den xml File --> speichere diesen in Variable xmlFile
         File xmlFile = new File("src/test/resources/person.xml");
 
+        //Initialisierung von den JAXBContext als Person-Klasse und Unmarshaller Objekten
         JAXBContext context = JAXBContext.newInstance(Person.class);
         Unmarshaller unmarshaller = context.createUnmarshaller();
 
+        //unmarshaller liest den xmlFile aus und speichert diesen als Person
         Person person = (Person) unmarshaller.unmarshal(xmlFile);
 
         System.out.println(person);
@@ -64,15 +67,20 @@ public class TestXml {
     @Test
     public void testPersonToXML() throws JAXBException{
 
+        //Erstelle person mit der createPerson() Methode
         Person person = createPerson();
 
+        //Initialisiere JAXB Context als Person-objekt und Initialisiere Marshaller
         JAXBContext context = JAXBContext.newInstance(Person.class);
         Marshaller marshaller = context.createMarshaller();
 
+        //formatierung für den zu speicherenden xml file
         marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, Boolean.TRUE);
 
+        //erstelle neuen file für den path
         File out = new File("src/test/resources/out.xml");
 
+        //nutzt marshaller.marshal(zu_speicherende_objekt, pathname)
         marshaller.marshal(person, out);
 
         System.out.println("XML File created" + out.getAbsolutePath());
